82c4c83170b5faf3e2ba77a7fa12793a045e12f3
bk <b2kgizachew@gmail.com>
2022-10-13 20:27:05 -0700

    C program that prints to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -entry
+ *
+ *
+ * Return: 1 (not success)
+ *
+ */
+int main()
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",63);
+	 return (1);
+
+}
