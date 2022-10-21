1b9193e8726643cf49ed255adbe4c51697e44134
bk <b2kgizachew@gmail.com>
2022-10-21 05:19:36 -0700

    printing lines

---
+++ 0x04-more_functions_nested_loops/6-print_lines.c
@@ -0,0 +1,20 @@
+/**
+ *
+ * print_line - print lines.
+ *
+ *
+ * Return : No it is void.
+ * 
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	for(i=n; i > 0; i++)
+		_putchar('_');
+	
+	_putchar('\n');
+		
+		
+}
