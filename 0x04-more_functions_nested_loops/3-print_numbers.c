30472ccc9bda91ae2a20e750628ce81f91759740
bk <b2kgizachew@gmail.com>
2022-10-21 04:36:58 -0700

    print num from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ *
+ * print_numbers - list 0-9.
+ *
+ *
+ * Return : No it is void.
+ * 
+ */
+
+void print_numbers(void)
+{
+	int i=0;
+	int end=10;
+	for(; i< end; i++)
+	       _putchar(i+'0');
+	_putchar('\n');	
+}
