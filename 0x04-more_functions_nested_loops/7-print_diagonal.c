457739e2c8d0f51dc74776eaa21417329a3687de
bk <b2kgizachew@gmail.com>
2022-10-21 05:26:52 -0700

    printing line diagonaly

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+/**
+ *
+ * print_diagonal - print '\' n-times.
+ *
+ *
+ * Return : No it is void.
+ * 
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	
+	for(i=n; i > 0; i++)
+		_putchar('\\');
+	
+	_putchar('\n');
+		
+		
+}
