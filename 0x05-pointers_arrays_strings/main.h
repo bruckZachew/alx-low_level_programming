75722506b20c2ad74ce5ea72f1fac923b06b5c25
bk <b2kgizachew@gmail.com>
2022-10-24 10:41:07 -0700

    adding main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+/**
+ *
+ *
+ * Function Declaration
+ */
+void reset_to_98(int*);
+void swap_int(int*,int*);
+int _strlen(char*);
+int _putchar(char);
+void _puts(char*);
+void print_rev(char*);
+void rev_string(char*);
+void puts2(char*);
+void puts_half(char*);
+void print_array(int*,int);
+char* _strcpy(char*,char*);
+int _atoi(char*);
