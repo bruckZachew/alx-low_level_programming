c40d12cdb4a8d59c62459b8e3c425b874c4082f0
bk <b2kgizachew@gmail.com>
2022-10-27 09:33:42 -0700

    adding main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+/**
+ *
+ *
+ *
+ * Function Declaration
+ */
+
+char* _strcat(char*,char*);
+char* _strncat(char*,char*,int);
+char* _strncpy(char*,char*,int);
+int _strcmp(char*,char*);
+void reverse_array(int*,int);
+char* string_toupper(char*);
+char* cap_string(char*);
+char* leet(char*);
+char* rot13(char*);
+void print_number(int);
+int _putchar(char);
